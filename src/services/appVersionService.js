bb5802c5e7b7dd7f808a03703049abcf898ab454
Kürşat <kursatmudu1@gmail.com>
2025-08-22 14:25:08 +0300

    Playstore-Version-API

--- src/services/appVersionService.js
+++ src/services/appVersionService.js
@@ -3,10 +3,10 @@ const gplay = require("google-play-scraper").default;
 class AppVersionService {
   async getAppInfo(appId) {
     if (!appId) {
-      throw new Error("appId parametresi gerekli");
+      throw new Error("appId is required");
     }
-    return await gplay.app({ appId });
+    const gplay = await import("google-play-scraper");
+    return gplay.default.app({ appId });
   }
 }
-
 module.exports = new AppVersionService();
